e226e61815eba77a5a10d32374fdaa3cc8db3007
beopop <beohosting.com@gmail.com>
2025-08-11 23:42:55 +0200

    feat: bootstrap laravel skeleton

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    public function test_home_page(): void
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+}
